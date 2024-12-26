bd17291ae218bfb2e9c941d678145fb42102d5e9
Dela Cruz, Clarence Kieth M. <clarencekiethdelacruz@gmail.com>
2024-12-26 21:15:39 +0800

    08-usestate-hook created a simple counter using stateful variable

---
+++ my-react-app/src/08-usestate-hook/Counter.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+import style from './Counter.module.css'
+
+function Counter(){
+
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+
+    const decrement = () => {
+        setCount(count - 1)
+    }
+
+    const reset = () => {
+        setCount(0)
+    }
+
+    return(
+        <div className={style.counterContainer}>
+            <p>{count}</p>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={reset}>Reset</button>
+            <button onClick={increment}>Increment</button>
+        </div>
+    )
+}
+
+export default Counter
